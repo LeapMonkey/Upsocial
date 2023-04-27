418c897c5a50e9bc2b6d3b8dd0577272a5fc423d
LeapMonkey <leapfrog2024@gmail.com>
2023-04-27 22:27:19 +0200

    config api

--- components/config/config.js
+++ components/config/config.js
@@ -1,2 +1,2 @@
 export const API_UPLOAD_URL = "https://upload63f6f713447ec.cloud.bunnyroute.com/api";
-export const apiURL = "https://upload63f6f713447ec.cloud.bunnyroute.com/";
+export const apiURL = "https://upload63f6f713447ec.cloud.bunnyroute.com";
